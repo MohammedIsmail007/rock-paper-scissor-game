6bdb8c9fd12b62d408dfbe170d11f34f1edb2ada
mohammedismail007 <mohammedismail9788@gmail.com>
2024-01-06 11:19:54 +0530

    rps game by S Mohammed Ismail @beginner_project_Js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class', '[data-mode="dark"]'],
+  
+  content: [
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
